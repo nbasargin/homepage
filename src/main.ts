33bc1463bbbb3d380524d5a2bf107acd36339296
Nikita Basargin <nbasargin@web.de>
2024-12-08 21:46:54 +0100

    inline config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app/app.routes";
+import { AppComponent } from "./app/app.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+  ],
+}).catch((err) => console.error(err));
