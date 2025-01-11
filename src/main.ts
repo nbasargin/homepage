89a7971b044a8e1fe1a6e7723e843a851d879e57
Nikita Basargin <nbasargin@web.de>
2025-01-11 19:14:52 +0100

    add angular material

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { provideExperimentalZonelessChangeDetection } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
-import { provideRouter } from "@angular/router";
-import { routes } from "./app/app.routes";
-import { AppComponent } from "./app/app.component";
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes),
-  ],
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimationsAsync()],
 }).catch((err) => console.error(err));
