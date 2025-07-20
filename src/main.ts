f33687a6d031c304697e29ca602c6cb78a6347c8
Nikita Basargin <nbasargin@web.de>
2025-07-20 19:36:31 +0200

    scroll position

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
+    provideAnimationsAsync(),
+  ],
 }).catch((err) => console.error(err));
