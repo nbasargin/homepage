d943342c7ca45fd66f328b7ec367766daf7236fa
Nikita Basargin <nbasargin@web.de>
2025-07-20 10:10:14 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'hp-app',
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   template: `
     <hp-header></hp-header>
     <router-outlet></router-outlet>
+    <hp-footer></hp-footer>
   `,
   styleUrl: './app.component.css',
 })
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hp-footer',
+  imports: [],
+  template: ` <div class="footer"></div> `,
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {}
