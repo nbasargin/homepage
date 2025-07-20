3165c8c3a46f0c13c8ded4476405b8dd6694bacc
Nikita Basargin <nbasargin@web.de>
2025-07-20 10:03:19 +0200

    adjust layout in the web surfels component

--- src/app/projects/web-surfels/web-surfels.component.ts
+++ src/app/projects/web-surfels/web-surfels.component.ts
@@ -1,36 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'hp-web-surfels',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   template: `
     <div class="block-title">WebSurfels</div>
 
-    <div>Some infos about the web surfels project</div>
     <div>
-      <a href="https://nbasargin.github.io/WebSurfels-Demo/"> Demo </a>
-      <a href="https://github.com/nbasargin/WebSurfels"> Github </a>
+      Master's thesis project, rendering of point clouds in the browser, focus on high quality rendering. Smooth color
+      blending is achieved with 2D surface splatting. Each point is rendered as a disc and the colors are blended
+      together for adjacent discs.
+    </div>
+    <div>
+      <a mat-button="elevated" href="https://nbasargin.github.io/WebSurfels-Demo/" target="_blank">Demo</a>
+      <a mat-button="elevated" href="https://github.com/nbasargin/WebSurfels" target="_blank">GitHub</a>
     </div>
 
+    <div class="block-subtitle">Neuschwanstein Castle Demo</div>
     <div class="image-container">
-      <img src="/assets/stable-diffusion-iceland.jpg" class="img-3-columns img-rounded" alt="image" />
-      <img src="/assets/stable-diffusion-forest-shrooms.jpg" class="img-3-columns img-rounded" alt="image" />
-      <img src="/assets/stable-diffusion-iceland.jpg" class="img-3-columns img-rounded" alt="image" />
+      <img src="/assets/stable-diffusion-iceland.jpg" class="img-rounded img-h400" alt="image" />
     </div>
-    <div class="image-caption">some text here that describes the images above</div>
+    <div>Render a large dataset progressively by loading finer levels of detail as the user zooms in.</div>
 
+    <div class="block-subtitle">Stanford Dragon Demo</div>
+    <div class="image-container">
+      <img src="/assets/stable-diffusion-iceland.jpg" class="img-rounded img-h400" alt="image" />
+    </div>
+    <div>Load a PLY file, construct a level of detail tree in the browser, and render it.</div>
+
+    <div class="block-subtitle">Google Street View Demo</div>
+    <div class="image-container">
+      <img src="/assets/stable-diffusion-iceland.jpg" class="img-rounded img-h400" alt="image" />
+    </div>
     <div>
-      general description: Master's thesis project, rendering of point clouds in the browser, focus on high quality
-      rendering. Smooth color blending is achieved with 2D surface splatting. Each point is rendered as a disc and the
-      colors are blended together for adjacent discs.
+      Load Google Street View panoramas and depth data, construct a point cloud from the data. Then, crawl adjacent
+      panoramas and expand the point cloud to reconstruct parts of the city. Note: the street view API has changed since
+      2020, so the demo is running on the old cached version of the data.
     </div>
 
-    <div>Neuschwanstein</div>
-    <div>Dragon</div>
-    <div>Point Cloud from Google Street View</div>
-    <div>Sphere</div>
+    <div class="block-subtitle">Sphere Demo</div>
+    <div class="image-container">
+      <img src="/assets/stable-diffusion-iceland.jpg" class="img-rounded img-h400" alt="image" />
+    </div>
+    <div>Render a sphere with a variable number of points. Mainly used as performance test.</div>
   `,
   host: {
     class: 'page-width',
