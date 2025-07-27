13b77cc5a6a4c6e4d0eaa9cde89ef69186d17de2
Nikita Basargin <nbasargin@web.de>
2025-07-27 12:57:04 +0200

    start sections for roboy

--- src/app/projects/roboy-cognition.ts
+++ src/app/projects/roboy-cognition.ts
@@ -26,11 +26,47 @@ export const roboyCognitionDescription: ProjectDescription = {
       </a>
     </div>
 
-    <div class="block-subtitle">Subtitle</div>
+    <div class="block-subtitle">Roboy Dialog</div>
     <div class="image-container">
       <img src="/assets/placeholder.png" class="img-rounded img-h400" alt="image" />
     </div>
-    <p>Details</p>
+    <p>Winter semester 2017-2018, Dialog team, later Ravestate</p>
+    <p>
+      Mid-semester events: 2017-10-20 TUM & Roboy, Garching, 2017-12-14 Roboy & Makerspace Weihnachtsparty, 2018-01-24
+      Roboy WS1718 Midterm & Party
+    </p>
+    <p>
+      Mid-semester events: 2018-03 Roboy Misc, 2018-03-14+ Roboy, KI Konferenz, 2018-03-22 Roboy Endterm WS1718,
+      2018-03-29 Roboy Hotpot
+    </p>
+
+    <div class="block-subtitle">Supervising Student teams</div>
+    <div class="image-container">
+      <img src="/assets/placeholder.png" class="img-rounded img-h400" alt="image" />
+    </div>
+    <p>Summer semester 2018: supervising teams, participating at exhibitions</p>
+    <p>Team supervision: BotBoy & TalkToTwo</p>
+    <p>2018-04-25 Roboy, Hannover</p>
+    <p>2018-06-21 Automatica</p>
+    <p>Mid-semester events: 2018-04 Roboy & UTUM - nice pics, 2018-06 Roboy & UTUM - safe ftopus</p>
+
+    <div class="block-subtitle">Later activities</div>
+    <div class="image-container">
+      <img src="/assets/placeholder.png" class="img-rounded img-h400" alt="image" />
+    </div>
+    <p>Exchange semester, no active participation at roboy for a while</p>
+    <p>Visiting and helping with specific events</p>
+    <p>
+      2019-03-07 Roboy Riksha Finals, 2019-05-05 Roboy Hackathon, 2019-07-18 Roboy Finals, 2019-07-26 Roboy Rave Party
+    </p>
+    <p>2019-09-06+ Roboy Finals SS19 & Techfest, 2020-03-09 Roboy, 2020-09-05 Roboy Finals SS20</p>
+
+    <p>
+      Summary of the Ravestate dialog system: https://arxiv.org/abs/2310.01943, my contribution: web-based interface for
+      real time visualization of state changes and transitions
+    </p>
+
+    <p>Roboy 2.0 in Deutsches Museum</p>
   `,
   host: {
     class: 'page-w800',
--- src/app/projects/roboy-middleware.ts
+++ src/app/projects/roboy-middleware.ts
@@ -16,7 +16,7 @@ export const roboyMiddlewareDescription: ProjectDescription = {
   selector: 'hp-projects-roboy-middleware',
   imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   template: `
-    <div class="block-title">Roboy Middleware</div>
+    <div class="block-title">Roboy - Middleware Team</div>
 
     <p>Roboy Middleware description</p>
     <div class="button-row">
@@ -26,11 +26,28 @@ export const roboyMiddlewareDescription: ProjectDescription = {
       </a>
     </div>
 
-    <div class="block-subtitle">Subtitle</div>
+    <div class="block-subtitle">HackRoboy 2</div>
     <div class="image-container">
       <img src="/assets/placeholder.png" class="img-rounded img-h400" alt="image" />
     </div>
-    <p>Details</p>
+    <p>Summer Semester 2017 kick-off hackathon, Nerfinators team (2017-04-28+ HackRoboy 2 - Nerfinators)</p>
+    <p>Before: 2017-02-09 Roboy meetup roboy v1</p>
+
+    <div class="block-subtitle">Lighthouse Tracking</div>
+    <div class="image-container">
+      <img src="/assets/placeholder.png" class="img-rounded img-h400" alt="image" />
+    </div>
+    <p>Summer Semester 2017 main project: lighthouse tracking</p>
+    <p>
+      Mid-semester events: 2017-05-03 MakerSpace, 2017-06-03 Roboy Grillen, 2017-06-26 Roboy, 2017-07 Roboy & MakerSpace
+    </p>
+
+    <div class="block-subtitle">Robotics Lab Tour</div>
+    <div class="image-container">
+      <img src="/assets/placeholder.png" class="img-rounded img-h400" alt="image" />
+    </div>
+    <p>After summer semester, visiting several robotics labs in Germany, France, and Switzerland</p>
+    <p>2017-09-25 Roboy, Heidelberg etc</p>
   `,
   host: {
     class: 'page-w800',
