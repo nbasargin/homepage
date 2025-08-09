b8ae23761d1647f1657a0e2d47728cd613c72d3b
Nikita Basargin <nbasargin@web.de>
2025-08-09 17:07:43 +0200

    polishing main layout

--- src/app/page-home/page-home.ts
+++ src/app/page-home/page-home.ts
@@ -7,7 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
   selector: 'hp-page-home',
   imports: [ProjectCardComponent, MatIconModule],
   template: `
-    <div class="grid-800 section-vertical-padding">
+    <div class="grid-800 pt-l pb-l">
       <div class="center-col">
         <div class="profile-block">
           <div class="personal-info">
@@ -23,9 +23,10 @@ import { MatIconModule } from '@angular/material/icon';
         </div>
       </div>
     </div>
-    <div class="grid-1200 projects-section section-vertical-padding">
+
+    <div class="projects-section grid-1200 pt-l pb-l">
       <div class="center-col">
-        <div class="block-title section-centered-title">Selected Projects</div>
+        <div class="block-title centered mb-l">Selected Projects</div>
         <div class="project-list">
           @for (project of HIGHLIGHT_PROJECTS; track $index) {
             <hp-project-card [projectData]="project"></hp-project-card>
@@ -33,76 +34,146 @@ import { MatIconModule } from '@angular/material/icon';
         </div>
       </div>
     </div>
-    <div class="grid-800 section-vertical-padding">
+
+    <div class="grid-800 pt-l pb-l">
       <div class="center-col">
-        <div class="block-title block-margin-bottom">Publications</div>
-        <div class="about-grid block-margin-bottom">
+        <div class="block-title mb-m">Publications</div>
+        <div class="about-grid">
           <div>
-            <div>Explainable Physical PolSAR Autoencoders for Soil Moisture Estimation</div>
-            <div>Nikita Basargin, Alberto Alonso-González, Irena Hajnsek</div>
+            <div class="item-title">Explainable Physical PolSAR Autoencoders for Soil Moisture Estimation</div>
+            <div class="authors">Nikita Basargin, Alberto Alonso-González, Irena Hajnsek</div>
             <div class="location-date">
-              <mat-icon fontIcon="location_on"></mat-icon> CVPR Workshops
-              <mat-icon fontIcon="date_range"></mat-icon> 2025
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>CVPR Workshops</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2025</span>
             </div>
           </div>
           <div>
-            <div>Constrained Tensor Decompositions for SAR Data: Agricultural Polarimetric Time Series Analysis</div>
-            <div>Nikita Basargin, Alberto Alonso-González, Irena Hajnsek</div>
-            <div>IEEE TGRS, 2023</div>
+            <div class="item-title">
+              Constrained Tensor Decompositions for SAR Data: Agricultural Polarimetric Time Series Analysis
+            </div>
+            <div class="authors">Nikita Basargin, Alberto Alonso-González, Irena Hajnsek</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>IEEE TGRS</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2023</span>
+            </div>
           </div>
         </div>
+      </div>
+    </div>
 
-        <div class="block-title block-margin-bottom">Scholarships and Awards</div>
-        <div class="about-grid block-margin-bottom">
+    <div class="awards-section grid-800 pt-l pb-l">
+      <div class="center-col">
+        <div class="block-title mb-m">Awards and Scholarships</div>
+        <div class="about-grid">
           <div>
-            <div>Best Poster Award</div>
-            <div>IEEE GRSS IADF School, 2023</div>
+            <div class="item-title">Best Poster Award</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>IEEE GRSS IADF School</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2023</span>
+            </div>
           </div>
           <div>
-            <div>Rohde & Schwarz Best-Bachelor-Award</div>
-            <div>TUM, 2017</div>
+            <div class="item-title">Rohde & Schwarz Best-Bachelor-Award</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>TUM</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2017</span>
+            </div>
           </div>
           <div>
-            <div>best.in.tum, best students of the Department of Informatics</div>
-            <div>2016 - 2020</div>
+            <div class="item-title">best.in.tum</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>TUM</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2016 - 2020</span>
+            </div>
           </div>
           <div>
-            <div>Max Weber Program (MWP) Scholarship</div>
-            <div>2014 - 2020</div>
+            <div class="item-title">Max Weber Program (MWP) Scholarship</div>
+            <div class="location-date">
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2014 - 2020</span>
+            </div>
           </div>
         </div>
+      </div>
+    </div>
 
-        <div class="block-title block-margin-bottom">Research & Development Experience</div>
-        <div class="about-grid block-margin-bottom">
+    <div class="grid-800 pt-l pb-l">
+      <div class="center-col">
+        <div class="block-title mb-m">Research & Development Experience</div>
+        <div class="about-grid">
           <div>
-            <div>Software Engineer</div>
-            <div>Userlane, Munich, 2019 - 2021</div>
+            <div class="item-title">Software Engineer</div>
             <div>Web development (Angular & React), Data analytics</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>Userlane, Munich</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2019 - 2021</span>
+            </div>
           </div>
         </div>
+      </div>
+    </div>
 
-        <div class="block-title block-margin-bottom">Education</div>
-        <div class="about-grid block-margin-bottom">
+    <div class="education-section grid-800 pt-l pb-l">
+      <div class="center-col">
+        <div class="block-title mb-m">Education</div>
+        <div class="about-grid">
           <div>
-            <div>PhD in Data Science and Remote Sensing</div>
-            <div>DLR, TUM, MUDS, Munich, 2021 - now</div>
+            <div class="item-title">PhD in Data Science and Remote Sensing</div>
             <div>Topic: Bio- and Geophysical Parameter Estimation from Multidimensional SAR Data</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>DLR, TUM, MUDS, Munich</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2021 - now</span>
+            </div>
           </div>
 
           <div>
-            <div>Master of Informatics</div>
-            <div>TUM, Munich, 2017 - 2020</div>
-            <div>UOW, Australia, 2018</div>
+            <div class="item-title">Master of Informatics</div>
             <div>Passed with high distinction, grade: 1.0</div>
             <div>Thesis: High-Quality Point Cloud Rendering in WebGL</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>TUM, Munich</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2017 - 2020</span>
+            </div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>UOW, Australia</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2018</span>
+            </div>
           </div>
 
           <div>
-            <div>Bachelor of Informatics</div>
-            <div>TUM, Munich, 2014 - 2017</div>
-            <div>NUS, Singapore, 2016</div>
+            <div class="item-title">Bachelor of Informatics</div>
             <div>Passed with high distinction, grade: 1.1</div>
             <div>Thesis: Interactive simulation of floods using the SPH method</div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>TUM, Munich</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2014 - 2017</span>
+            </div>
+            <div class="location-date">
+              <mat-icon fontIcon="location_on"></mat-icon>
+              <span>NUS, Singapore</span>
+              <mat-icon fontIcon="date_range"></mat-icon>
+              <span>2016</span>
+            </div>
           </div>
         </div>
       </div>
