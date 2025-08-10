73383ecf720acf5448982481b40ecaf839faa9d4
Nikita Basargin <nbasargin@web.de>
2025-08-10 17:54:37 +0200

    zoomable images in physical-autoencoders

--- src/app/projects/physical-autoencoders.ts
+++ src/app/projects/physical-autoencoders.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ProjectDescription } from './project-description';
 import { ListEntryComponent } from '../list-entry/list-entry';
+import { GalleryComponent, GalleryImage } from '../gallery/gallery';
 
 export const physicalAutoencodersDescription: ProjectDescription = {
   title: 'Physical Autoencoders',
@@ -15,7 +16,7 @@ export const physicalAutoencodersDescription: ProjectDescription = {
 
 @Component({
   selector: 'hp-projects-physical-autoencoders',
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, ListEntryComponent],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, ListEntryComponent, GalleryComponent],
   template: `
     <div class="grid-800 pt-l pb-l">
       <div class="center-col">
@@ -49,33 +50,23 @@ export const physicalAutoencodersDescription: ProjectDescription = {
           and predict the radar signal given a set of geophysical parameters (e.g. soil moisture).
         </p>
 
-        <div class="image-container">
-          <img
-            src="/assets/projects/physical-autoencoders-physics.png"
-            class="img-rounded img-h200"
-            alt="Physical model"
-          />
-        </div>
+        <hp-gallery [images]="physicalImage"></hp-gallery>
+
         <p>
           In contrast, supervised ML methods are commonly trained to predict geophysical parameters by learning the
           patterns from large datasets.
         </p>
-        <div class="image-container">
-          <img src="/assets/projects/physical-autoencoders-ml.png" class="img-rounded img-h200" alt="ML model" />
-        </div>
+
+        <hp-gallery [images]="mlImage"></hp-gallery>
+
         <p>
           The main idea of this paper is to combine ML with physical models into a single autoencoder architecture. An
           ML encoder projects the data into a physical latent space, where each latent parameter has a physical meaning
           (e.g. soil moisture, surface intensity, etc.). The latent parameters are then passed through a differentiable
           physics-based decoder to reconstruct the original signal.
         </p>
-        <div class="image-container">
-          <img
-            src="/assets/projects/physical-autoencoders-combined.png"
-            class="img-rounded img-h200"
-            alt="Physical autoencoder"
-          />
-        </div>
+
+        <hp-gallery [images]="combinedImage"></hp-gallery>
 
         <p>
           This approach creates a fully explainable latent space, provides estimates for the physical parameters, and
@@ -88,23 +79,85 @@ export const physicalAutoencodersDescription: ProjectDescription = {
 
         <p>The conference took place in Nashville, Tennessee, USA. Here are some impressions.</p>
 
-        <div class="image-container">
-          <img
-            src="/assets/projects/physical-autoencoders-cvpr-01.jpg"
-            class="img-rounded img-h250"
-            alt="Social event"
-          />
-          <img src="/assets/projects/physical-autoencoders-cvpr-02.jpg" class="img-rounded img-h250" alt="Tech demos" />
-          <img
-            src="/assets/projects/physical-autoencoders-cvpr-04.jpg"
-            class="img-rounded img-h250"
-            alt="Poster session"
-          />
-          <img src="/assets/projects/physical-autoencoders-cvpr-03.jpg" class="img-rounded img-h250" alt="Keynotes" />
-        </div>
+        <hp-gallery [images]="cvprImages"></hp-gallery>
       </div>
     </div>
   `,
   styles: '',
 })
-export class PhysicalAutoencodersComponent {}
+export class PhysicalAutoencodersComponent {
+  physicalImage: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-physics.1000x500.png',
+      imgWidth: 1000,
+      imgHeight: 500,
+      thumbSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-physics.1000x500.png',
+      thumbAlt: 'Physical forward model',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+  ];
+
+  mlImage: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-ml.1000x500.png',
+      imgWidth: 1000,
+      imgHeight: 500,
+      thumbSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-ml.1000x500.png',
+      thumbAlt: 'ML model',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+  ];
+
+  combinedImage: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-combined.1500x500.png',
+      imgWidth: 1500,
+      imgHeight: 500,
+      thumbSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-combined.1500x500.png',
+      thumbAlt: 'Physical autoencoder',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+  ];
+
+  cvprImages: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-01.4080x3072.jpg',
+      imgWidth: 4080,
+      imgHeight: 3072,
+      thumbSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-01.664x500.jpg',
+      thumbAlt: 'Social event',
+      thumbClass: 'img-rounded img-h250',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-02.3402x2562.jpg',
+      imgWidth: 3402,
+      imgHeight: 2562,
+      thumbSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-02.664x500.jpg',
+      thumbAlt: 'Tech demos',
+      thumbClass: 'img-rounded img-h250',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-03.4080x3072.jpg',
+      imgWidth: 4080,
+      imgHeight: 3072,
+      thumbSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-03.664x500.jpg',
+      thumbAlt: 'Poster session',
+      thumbClass: 'img-rounded img-h250',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-04.2651x1996.jpg',
+      imgWidth: 2651,
+      imgHeight: 1996,
+      thumbSrc: '/assets/projects/physical-autoencoders/physical-autoencoders-cvpr-04.664x500.jpg',
+      thumbAlt: 'Keynotes',
+      thumbClass: 'img-rounded img-h250',
+      thumbCropped: false,
+    },
+  ];
+}
