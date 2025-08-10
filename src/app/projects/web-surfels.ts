3c9c4b053f15ade7621153d5fe87993dc557595a
Nikita Basargin <nbasargin@web.de>
2025-08-10 15:34:15 +0200

    zoomable images in web-surfels

--- src/app/projects/web-surfels.ts
+++ src/app/projects/web-surfels.ts
@@ -4,17 +4,18 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ProjectDescription } from './project-description';
+import { GalleryComponent, GalleryImage } from '../gallery/gallery';
 
 export const webSurfelsDescription: ProjectDescription = {
   title: 'WebSurfels',
   description: 'High-Quality Point Cloud Rendering in WebGL',
-  imageUrl: '/assets/projects/web-surfels-preview.jpg',
+  imageUrl: '/assets/projects/web-surfels/web-surfels-preview.700x400.jpg',
   detailsLink: '/web-surfels',
 };
 
 @Component({
   selector: 'hp-projects-web-surfels',
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, GalleryComponent],
   template: `
     <div class="grid-800 pt-l pb-l">
       <div class="center-col">
@@ -35,7 +36,7 @@ export const webSurfelsDescription: ProjectDescription = {
           </a>
           <a mat-flat-button href="https://github.com/nbasargin/WebSurfels" target="_blank">
             <mat-icon svgIcon="github"></mat-icon>
-            GitHub
+            Code
           </a>
         </div>
 
@@ -45,18 +46,9 @@ export const webSurfelsDescription: ProjectDescription = {
           structure is constructed from the original data in a preprocessing step using an octree and stores LOD
           representations at different resolutions.
         </p>
-        <div class="image-container">
-          <img
-            src="/assets/projects/web-surfels-neuschwanstein-01-h500.jpg"
-            class="img-rounded img-h250"
-            alt="Rendered Neuschwanstein Castle 1"
-          />
-          <img
-            src="/assets/projects/web-surfels-neuschwanstein-02-h500.jpg"
-            class="img-rounded img-h250"
-            alt="Rendered Neuschwanstein Castle 2"
-          />
-        </div>
+
+        <hp-gallery [images]="neuschwansteinImages"></hp-gallery>
+
         <p>
           The Neuschwanstein Castle dataset contains more than 30 million points. The data is streamed to the client on
           demand, reducing the loading times. Only the necessary parts of the scene are rendered in high resolution to
@@ -64,41 +56,18 @@ export const webSurfelsDescription: ProjectDescription = {
         </p>
 
         <div class="block-subtitle mt-l mb-m">Stanford Dragon Demo</div>
-        <div class="image-container">
-          <img
-            src="/assets/projects/web-surfels-dragon-01-h500.jpg"
-            class="img-rounded img-h200"
-            alt="Low-resolution dragon"
-          />
-          <img
-            src="/assets/projects/web-surfels-dragon-02-h500.jpg"
-            class="img-rounded img-h200"
-            alt="Medium-resolution dragon"
-          />
-          <img
-            src="/assets/projects/web-surfels-dragon-03-h500.jpg"
-            class="img-rounded img-h200"
-            alt="High-resolution dragon"
-          />
-        </div>
+
+        <hp-gallery [images]="dragonImages"></hp-gallery>
+
         <p>
           The Stanford Dragon demo showcases construction of a level-of-detail tree from a PLY file directly in the
           browser. Here, 1210, 27869, and 435545 splats are rendered for different levels-of-detail.
         </p>
 
         <div class="block-subtitle mt-l mb-m">Google Street View Demo</div>
-        <div class="image-container">
-          <img
-            src="/assets/projects/web-surfels-street-view-01-h500.jpg"
-            class="img-rounded img-h200"
-            alt="Rendered Street View 1"
-          />
-          <img
-            src="/assets/projects/web-surfels-street-view-02-h500.jpg"
-            class="img-rounded img-h200"
-            alt="Rendered Street View 2"
-          />
-        </div>
+
+        <hp-gallery [images]="streetViewImages"></hp-gallery>
+
         <p>
           The Google Street View demo demonstrates the integration of the renderer with an existing service. The demo
           loads the publicly available data from the Google Street View API and reconstructs local point clouds from
@@ -110,23 +79,9 @@ export const webSurfelsDescription: ProjectDescription = {
         </p>
 
         <div class="block-subtitle mt-l mb-m">Sphere Demo</div>
-        <div class="image-container">
-          <img
-            src="/assets/projects/web-surfels-sphere-01-h500.jpg"
-            class="img-rounded img-h200"
-            alt="Rendered Sphere 1"
-          />
-          <img
-            src="/assets/projects/web-surfels-sphere-02-h500.jpg"
-            class="img-rounded img-h200"
-            alt="Rendered Sphere 2"
-          />
-          <img
-            src="/assets/projects/web-surfels-sphere-03-h500.jpg"
-            class="img-rounded img-h200"
-            alt="Rendered Sphere 3"
-          />
-        </div>
+
+        <hp-gallery [images]="sphereImages"></hp-gallery>
+
         <p>
           The sphere demo renders a sphere with a variable number of points, splat sizes, and color blending options.
           The demo is mainly used for performance testing and visual quality comparisons.
@@ -134,21 +89,108 @@ export const webSurfelsDescription: ProjectDescription = {
       </div>
     </div>
   `,
-  styles: `
-    .image-row {
-      display: flex;
-      flex-wrap: wrap;
-    }
-    .image-row-block {
-      height: 40vh;
-      flex-grow: 1;
-    }
-    .image-row-block img {
-      max-height: 100%;
-      min-width: 100%;
-      object-fit: cover;
-      vertical-align: bottom;
-    }
-  `,
+  styles: '',
 })
-export class WebSurfelsComponent {}
+export class WebSurfelsComponent {
+  neuschwansteinImages: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-neuschwanstein-01.1492x1330.jpg',
+      imgWidth: 1492,
+      imgHeight: 1330,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-neuschwanstein-01.561x500.jpg',
+      thumbAlt: 'Rendered Neuschwanstein Castle',
+      thumbClass: 'img-rounded img-h250',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-neuschwanstein-02.1873x1330.jpg',
+      imgWidth: 1873,
+      imgHeight: 1330,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-neuschwanstein-02.704x500.jpg',
+      thumbAlt: 'Rendered Neuschwanstein Castle',
+      thumbClass: 'img-rounded img-h250',
+      thumbCropped: false,
+    },
+  ];
+
+  dragonImages: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-dragon-01.1380x1165.jpg',
+      imgWidth: 1380,
+      imgHeight: 1165,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-dragon-01.592x500.jpg',
+      thumbAlt: 'Low-resolution dragon, 1210 splats',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-dragon-02.1380x1165.jpg',
+      imgWidth: 1380,
+      imgHeight: 1165,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-dragon-02.592x500.jpg',
+      thumbAlt: 'Medium-resolution dragon, 27869 splats',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-dragon-03.1380x1165.jpg',
+      imgWidth: 1380,
+      imgHeight: 1165,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-dragon-03.592x500.jpg',
+      thumbAlt: 'High-resolution dragon, 435545 splats',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+  ];
+
+  streetViewImages: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-street-view-01.2079x1330.jpg',
+      imgWidth: 2079,
+      imgHeight: 1330,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-street-view-01.782x500.jpg',
+      thumbAlt: 'Point cloud reconstructed from Google Street View in Paris',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-street-view-02.1661x1063.jpg',
+      imgWidth: 1661,
+      imgHeight: 1063,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-street-view-02.781x500.jpg',
+      thumbAlt: 'Point cloud reconstructed from Google Street View in Paris',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+  ];
+
+  sphereImages: Array<GalleryImage> = [
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-sphere-01.1330x1330.jpg',
+      imgWidth: 1330,
+      imgHeight: 1330,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-sphere-01.500x500.jpg',
+      thumbAlt: 'Rendered Sphere, small splats, large gaps, no color blending',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-sphere-02.1330x1330.jpg',
+      imgWidth: 1330,
+      imgHeight: 1330,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-sphere-02.500x500.jpg',
+      thumbAlt: 'Rendered Sphere, large overlapping splats, no color blending',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+    {
+      imgSrc: '/assets/projects/web-surfels/web-surfels-sphere-03.1330x1330.jpg',
+      imgWidth: 1330,
+      imgHeight: 1330,
+      thumbSrc: '/assets/projects/web-surfels/web-surfels-sphere-03.500x500.jpg',
+      thumbAlt: 'Rendered Sphere, large splats with color blending',
+      thumbClass: 'img-rounded img-h200',
+      thumbCropped: false,
+    },
+  ];
+}
